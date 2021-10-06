311a0818a04b31eb039c0a1297a6ea72e6569aa7
alt-Pranav <74000887+alt-Pranav@users.noreply.github.com>
2021-10-06 13:45:53 +0530

    first commit to share style and basic format, will need modifications

---
+++ home_interact.js
@@ -0,0 +1,9 @@
+/**
+ * Select all the elements in the HTML page
+ * and assign a variable to each
+ */
+
+// within div class "card"
+let card_text = document.getElementById(".intro_text");
+let card_img = document.querySelector(".responsive");
+
